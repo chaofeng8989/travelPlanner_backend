73c8d5ae549d80af0108a83e61bbc845ee4702b1
chaofeng8989@gmail.com <fc198989>
2020-09-12 00:30:18 -0700

    adD googleMap client

--- src/main/java/team6/travelplanner/googleClient/MapClient.java
+++ src/main/java/team6/travelplanner/googleClient/MapClient.java
@@ -2,28 +2,34 @@ package team6.travelplanner.googleClient;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
+import com.google.maps.*;
 import com.google.maps.errors.ApiException;
-import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.*;
 import org.springframework.stereotype.Component;
 import team6.travelplanner.Vault;
+import team6.travelplanner.models.Place;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
-@Component
 public class MapClient {
     GeoApiContext context = new GeoApiContext.Builder()
             .apiKey(Vault.googleAPIKEY)
             .build();
-    GeocodingResult[] results;
 
-    {
+    public PlacesSearchResponse getNearbyPlacesNextPage(String nextPageToken) {
+        PlacesSearchResponse result = null;
         try {
-            results = GeocodingApi.geocode(context,
-                    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+
+            result = PlacesApi.nearbySearchNextPage(context, nextPageToken)
+                    .await();
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            System.out.println(gson.toJson(results[0].addressComponents));
+            System.out.println(gson.toJson(result));
         } catch (ApiException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -31,8 +37,84 @@ public class MapClient {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return result;
     }
 
 
+    public PlacesSearchResponse getNearbyPlaces(double lat, double lng) {
+        PlacesSearchResponse result = null;
 
+        try {
+
+            LatLng location = new LatLng(lat, lng);
+            result = PlacesApi.nearbySearchQuery(context, location)
+                    .radius(5000)
+                    .type(PlaceType.AMUSEMENT_PARK)
+                    .type(PlaceType.AQUARIUM)
+                    .type(PlaceType.ART_GALLERY)
+                    .type(PlaceType.TOURIST_ATTRACTION)
+                    .await();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            System.out.println(gson.toJson(result));
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public PlaceDetails getPlaceDetails(String placeId) {
+        PlaceDetails placeDetails = null;
+        try {
+             placeDetails = PlacesApi.placeDetails(context, placeId).await();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(placeDetails);
+        return placeDetails;
+    }
+    final String photoReference = "Photo Reference";
+
+    public ImageResult getPhotoUrl(String photoReference, int width, int height) {
+        ImageResult p = null;
+        try {
+            p = PlacesApi.photo(context, photoReference)
+                    .maxWidth(width)
+                    .maxHeight(height)
+                    .await();
+            System.out.println("    " + p);
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        MapClient client = new MapClient();
+        PlacesSearchResponse response = client.getNearbyPlaces(47.608013,  -122.335167);
+        System.out.println(response.nextPageToken);
+        Photo photo = null;
+
+        for (PlacesSearchResult p : response.results) {
+            client.getPlaceDetails(p.placeId);
+            ImageResult r = client.getPhotoUrl(p.photos[0].photoReference, 400, 400);
+            try {
+                Files.write(Path.of("src/main/resources/"+p.name+".jpg"), r.imageData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
