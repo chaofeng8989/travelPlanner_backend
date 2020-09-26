73ecd5da0b41d6762efbce475da05f31fb707a49
Shanchao Li <shanchao.li@care.com>
2020-09-25 21:59:13 -0700

    Login/Register

--- src/main/java/team6/travelplanner/controllers/UserController.java
+++ src/main/java/team6/travelplanner/controllers/UserController.java
@@ -3,6 +3,11 @@ package team6.travelplanner.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import team6.travelplanner.models.User;
 import team6.travelplanner.models.UserRepository;
@@ -12,11 +17,29 @@ import team6.travelplanner.models.UserRepository;
 public class UserController {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody User user) {
+        //Check if the same username is already registered
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return ResponseEntity.badRequest().body("UserName occupied");
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         log.info("user created successfully " + user.getUsername());
-        return ResponseEntity.accepted().build();
+        return ResponseEntity.ok("User successfully created");
+    }
+    @GetMapping("/loginfromgithub")
+    public ResponseEntity registerFromGithub(
+            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+            @AuthenticationPrincipal OAuth2User oAuth2User) {
+        if (userRepository.findByUsername(oAuth2User.getName()) == null) {
+            User user = new User();
+            user.setUsername(oAuth2User.getName());
+            userRepository.save(user);
+        }
+        return ResponseEntity.ok(oAuth2User.getName() + "Logged in from Github");
     }
 }
--- src/main/java/team6/travelplanner/security/SecurityConfig.java
+++ src/main/java/team6/travelplanner/security/SecurityConfig.java
@@ -4,41 +4,81 @@ import net.bytebuddy.asm.AsmVisitorWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired MyUserDetailsService userDetailsService;
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+public class SecurityConfig {
+
+    @Order(1)
+    @Configuration
+    public static class JWTConfig extends WebSecurityConfigurerAdapter {
+        @Autowired
+        MyUserDetailsService userDetailsService;
+        @Autowired
+        private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            //System.out.println("in auth config");
+            auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and().csrf().disable().authorizeRequests()
+                    .antMatchers("/register").permitAll()
+                    .anyRequest().authenticated()
+                    .and().formLogin()
+                    .loginPage("/login")
+                    .and()
+                    //.addFilter(new JWTAuthenticationFilter(authenticationManager()));
+                    .oauth2Login().defaultSuccessUrl("/loginfromgithub");
+                    // this disables session creation on Spring Security
+                    //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().anyRequest();
+        }
+
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        System.out.println("in auth config");
-        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
+
+
+    @Order(2)
+    @Configuration
+    public static class GitHubConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and().csrf().disable().authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2Login();
+        }
+        /*
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().anyRequest();
+        }
+        */
+
     }
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().anyRequest();
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().and().authorizeRequests().antMatchers("/register").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .logout().permitAll().logoutSuccessUrl("/login");
-    }
+
 
 }
