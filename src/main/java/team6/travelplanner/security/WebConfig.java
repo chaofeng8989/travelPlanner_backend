80930cd9a7b955162cd8bd48f3d2704b51ad2135
chao <chao@qianqiaz.onmicrosoft.com>
2020-10-04 20:59:37 +0000

    Merged PR 50: cors test
    cors test

--- src/main/java/team6/travelplanner/security/WebConfig.java
+++ src/main/java/team6/travelplanner/security/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**");
+                registry.addMapping("https://travelplannerbackend.azurewebsites.net/**");
             }
         };
     }
